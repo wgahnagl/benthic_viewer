795c328e00f9d3cd1b20bd17761cd697e3003051
Skyler Clark <skclark@redhat.com>
2025-04-06 22:43:44 -0400

    adds simple model

--- texture_editor/save_image.gd
+++ texture_editor/save_image.gd
@@ -3,7 +3,7 @@ extends Button
 class_name SaveButton
 @export var filename: String
 
-@onready var save_image_script = $"../PanelContainer/PixelEditor" # Ensure the correct node path
+@onready var save_image_script = $"../../PanelContainer/PixelEditor" # Ensure the correct node path
 func _ready():
 	print("SaveButton initialized with filename:", filename)
 	connect("pressed", Callable(self, "save_image"))
