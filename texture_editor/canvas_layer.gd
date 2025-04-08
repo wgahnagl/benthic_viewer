13e5387f9bf2ce7ef5b1adac80ffd9d7619a04f7
Skyler Clark <skclark@redhat.com>
2025-04-08 17:33:31 -0400

    adds fonts, starting to get styling

--- texture_editor/canvas_layer.gd
+++ texture_editor/canvas_layer.gd
@@ -7,31 +7,32 @@ var is_drawing = false
 var previous_pos = Vector2()
 
 
-@export var draw_area_position : Vector2 = Vector2(0, 0)  # Exported position of the draw area
+@export var draw_area_position : Vector2 = Vector2(250, 120)  # Exported position of the draw area
 @export var draw_area_size : Vector2 = Vector2(200, 200)  # Exported size of the draw area
 
 @onready var texture_rect : TextureRect = %TextureRect
-@onready var viewport : Viewport = $SubViewport
-@onready var full_rect: TextureRect = $"../../PanelContainer2/TextureRect"
+@onready var full_rect: TextureRect = $display
 @onready var camera : Camera2D = $SubViewport/Camera2D  # Assuming you have a Camera2D node in the SubViewport
 
 func _ready():
+	
 	# Create the image (1500x1500 texture)
 	image = Image.create(1500, 1500, false, Image.FORMAT_RGBA8)
-	image.fill(Color(0, 0, 0, 0))  # Fill the image with transparency
-
+	image.fill(Color(1, 1, 1, 1))  # Fill the image with transparency
+	texture_rect.set_position(-draw_area_position)
 	# Create the ImageTexture
 	texture = ImageTexture.create_from_image(image)
-	
 	# Set the TextureRect to display the 200x200 section
 	texture_rect.texture = texture
-	
+		
 	# Define the portion of the image to show in the TextureRect (200x200 region)
 	draw_area = Rect2(draw_area_position, draw_area_size)
-	viewport.size = draw_area_size
-	camera.position = draw_area_position
-	
-	full_rect.texture = texture  # Assign the full texture to the new TextureRect
+
+	var at = AtlasTexture.new()
+	at.atlas = texture
+	at.region = Rect2(draw_area_position, draw_area_size)
+
+	full_rect.texture = at  # Assign the full texture to the new TextureRect
 		# Set the stretch mode to scale the texture
 	full_rect.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
 	set_player_texture(texture)
@@ -59,7 +60,6 @@ func _input(event):
 			# Start drawing when the mouse button is pressed
 			var local_pos = texture_rect.get_local_mouse_position()
 			if draw_area.has_point(local_pos):
-				print("aaaaaaaa")
 				is_drawing = true
 				previous_pos = local_pos  # Set the previous position to the current mouse position
 				draw_at(local_pos)  # Draw at the starting point
@@ -99,9 +99,3 @@ func draw_between(start_pos: Vector2, end_pos: Vector2):
 	for step in range(num_steps):
 		var lerp_pos = start_pos.lerp(end_pos, step / float(num_steps))
 		draw_at(lerp_pos)  # Draw at the interpolated position
-
-func _draw():
-	# Draw the texture (the image we are updating) on the canvas
-	draw_texture(texture, Vector2.ZERO)
-	# Optional: Draw a border to indicate the valid drawing area
-	#draw_rect(draw_area.position, draw_area.size, Color(1, 1, 1, 0.3), false, 2.0)
